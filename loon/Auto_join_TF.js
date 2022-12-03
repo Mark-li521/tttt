8fbee06e3f8c4a9c4aa236d10a4c2c1afe410b97
李健 <mark@lijiandeMacBook-Pro.local>
2022-12-04 00:31:22 +0800

    test

--- loon/Auto_join_TF.js
+++ loon/Auto_join_TF.js
@@ -1,3 +1,4 @@
+console.log(1)
 !(async () => {
     ids = $persistentStore.read('APP_ID')
     if (ids == '') {
@@ -17,11 +18,11 @@ function autoPost(ID) {
     let testurl = 'https://testflight.apple.com/v3/accounts/' + Key + '/ru/'
     let request_url = {
         url: testurl + ID,
-        headers:
+        header:
             {
-                'X-Session-Id': `${$persistentStore.read('session_id')}`,
-                'X-Session-Digest': `${$persistentStore.read('session_digest')}`,
-                'X-Request-Id': `${$persistentStore.read('request_id')}`,
+               'X-Session-Id': `${$persistentStore.read('session_id')}`,
+               'X-Session-Digest': `${$persistentStore.read('session_digest')}`,
+               'X-Request-Id': `${$persistentStore.read('request_id')}`,
             }
     }
     console.log(request_url)
@@ -47,7 +48,7 @@ function autoPost(ID) {
                     } else {
                         let accept_url = {
                             url: testurl + ID + '/accept',
-                            headers:
+                            header:
                                 {
                                     'X-Session-Id': `${$persistentStore.read('session_id')}`,
                                     'X-Session-Digest': `${$persistentStore.read('session_digest')}`,
