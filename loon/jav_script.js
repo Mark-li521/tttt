53488d754e6bbea2fe3ef7f619c6f30248b1a80c
李健 <mark@lijiandeMacBook-Pro-2.local>
2024-08-14 23:08:31 +0800

    xxx

--- loon/jav_script.js
+++ loon/jav_script.js
@@ -1,23 +1,22 @@
-!(async () => {
-    let headers = $request.headers
-    let currentValue = "?authorization=" + headers['Proxy-Authorization'] + "&jdsignature=" + headers['jdsignature'];
-    let previousValue = $persistentStore.read(['jav_login']);
-    if (previousValue == currentValue) {
-        $notification.post('无需修改', '')
-    } else {
-        // 值发生变化，写入新值
-        await $persistentStore.write(['jav_login'], currentValue);
-        previousValue = currentValue;
-        // 调用接口
-        await callApi();
-    }
-    $done()
-})();
 
-async function callApi() {
+let headers = $request.headers;
+let currentValue = "?authorization=" + headers['Proxy-Authorization'] + "&jdsignature=" + headers['jdsignature'];
+let previousValue = $persistentStore.read(['jav_login']);
+if (previousValue == currentValue) {
+    $notification.post('无需修改', '');
+} else {
+    // 值发生变化，写入新值
+    await $persistentStore.write(['jav_login'], currentValue);
+    previousValue = currentValue;
+    // 调用接口
+    await callApi();
+}
+$done()
+
+function callApi() {
     try {
-        let previousValue = await $persistentStore.read(['jav_login']);
-        const response = await fetch('https://vod.markc.top:58588/get_login' + previousValue, {
+        let previousValue = $persistentStore.read(['jav_login']);
+        const response = fetch('https://vod.markc.top:58588/get_login' + previousValue, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -28,7 +27,7 @@ async function callApi() {
             throw new Error('Network response was not ok');
         }
 
-        const data = await response.json();
+        const data = response.json();
         console.log('API response:', data);
     } catch (error) {
         console.error('Error calling API:', error);
